f014fc508e39b5120661812017ead9adff97483f
Nishaj <njnishaj0@gmail.com>
2022-09-27 13:44:25 +0530

    changed js connected 'lis' to a array (not woking properly)

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 let input = document.querySelector("#input");
 let addBtn = document.querySelector("#add-btn")
 let ul = document.querySelector("#ul")
+let close = document.querySelector(".close")
+let li = document.querySelector("li")
+
+
+let liArray = [
+   " hello world "
+]
+
+
+let liReturn = function (item) {
+
+
+   for (i = 0; i < liArray.length; i++) {
+      let li = document.createElement("li")
+      li.append(liArray[i])
+      ul.append(li)
+      console.log(liArray[i]);
+
+   }
+}
+
 
 let addLi = () => {
    if (input.value) {
-      const liCreate = document.createElement("li");
+      liArray = [...liArray, input.value]
 
-      liCreate.append(input.value)
-      ul.append(liCreate)
+      if(!ul.value){
+         liReturn()
+      }
+      else{
+         li.remove()
+      }
+      
+
+      console.log(liArray)
    }
 };
