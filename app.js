0650bb279cf2303819ae12807f27ed5ace5a3a20
iambasilp <basilxg321@gmail.com>
2022-09-28 20:12:16 +0530

    deleting list

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const addbtn = document.querySelector('.addbtn')
-const Input = document.querySelector('.input')
-const TodoLists = document.querySelector('.todo-lists')
-function showMenu(){
-  let InputValue = Input.value
-  let li = document.createElement('li')
+const addbtn = document.querySelector(".addbtn");
+const Input = document.querySelector(".input");
+const TodoLists = document.querySelector(".todo-lists");
+function showMenu() {
+  let InputValue = Input.value;
+  let li = document.createElement("li");
   li.innerText = InputValue;
-  TodoLists.appendChild(li)
-  Input.value = ""
-
+  TodoLists.appendChild(li);
+  Input.value = "";
+  let trash = document.createElement("i");
+  trash.classList.add("ri-delete-bin-4-fill");
+  li.appendChild(trash);
 }
 
-addbtn.addEventListener('click',showMenu)
+// This is function for deleting list ,
+function deletelist(event) {
+  if (event.target.classList[0] == "ri-delete-bin-4-fill") {
+    let parent = event.target.parentElement;
+    parent.classList.add('slideDown')
+    parent.addEventListener("transitionend", () => {
+      let parent = event.target.parentElement
+      parent.remove();
+    });
+  }
+}
+
+TodoLists.addEventListener("click", deletelist);
+addbtn.addEventListener("click", showMenu);
